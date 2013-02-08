ee93d06f9fe765e8b8b0bb75e1a5893d0fcd0989
Andrea Crisafuli a.k.a. Xanio <xanio@nemesilabs.org>
2013-02-08 17:14:00 +0100

    +Agg. Wizard Contratto (step4) + css

--- addcontratti.php
+++ addcontratti.php
@@ -22,13 +22,14 @@ echo $menu;
 if(isset($_POST['step'])){
 	switch ($_POST['step']) {
 		case 1:
-			echo "<h3>Tipologia Contratto</h3>";
+			echo "<h3>STEP 1 - Creazione Contratto</h3>";
 			echo "<p>Seleziona la tipologia di contratto che si desidera attivare;</p>";
 			$sql = "SELECT * FROM Tipologie";
 			$res = mysql_query($sql);
 			
 			echo"<form action=\"addcontratti.php\" method=\"post\">
-			<fieldset>
+			<fieldset id=\"inputs\" style=\"border:1px solid #128F9A\">
+				<legend>Tipologia Contratto</legend><br />
 				<label>Tipologia</label>
 				<select name=\"TipologiaId\">
 				<option>...</option>
@@ -37,23 +38,24 @@ if(isset($_POST['step'])){
 					echo "<option value=\"".$rsTipologie['TipologiaId']."\">".$rsTipologie['TipologiaNome']."</option>";
 					}
 			echo"</select>
-			<input id=\"IdCliente\" name=\"IdCliente\" type=\"hidden\" value=\"".$_POST['IdCliente']."\" >
-			<input id=\"ClienteTipologia\" name=\"ClienteTipologia\" type=\"hidden\" value=\"".$_POST['ClienteTipologia']."\" >
-			<input id=\"step\" name=\"step\" type=\"hidden\" value=\"2\" >
-			</fieldset>
-			<fieldset id=\"actions\">
-            <input type=\"submit\" id=\"submit\" value=\"avanti\">
-            </fieldset>
+					<input id=\"IdCliente\" name=\"IdCliente\" type=\"hidden\" value=\"".$_POST['IdCliente']."\" >
+					<input id=\"ClienteTipologia\" name=\"ClienteTipologia\" type=\"hidden\" value=\"".$_POST['ClienteTipologia']."\" >
+					<input id=\"step\" name=\"step\" type=\"hidden\" value=\"2\" >
+				</fieldset>
+				<fieldset id=\"actions\">
+					<input type=\"submit\" id=\"submit\" value=\"avanti\">
+				</fieldset>
 			</form>";
 			break;
 		case 2:
-			echo "<h3>Offerta</h3>";
+			echo "<h3>STEP 2 - Creazione Contratto</h3>";
 			echo "<p>Seleziona l'offerta da associare al contratto;</p>";
 			if ($_POST['ClienteTipologia'] == 'Privato'){
 				$sql = "SELECT * FROM Offerte WHERE TipologiaId = '".$_POST['TipologiaId']."' and OffertaDestinazione = 'Privato'";
 				$res = mysql_query($sql);
 				echo"<form action=\"addcontratti.php\" method=\"post\">
-				<fieldset>
+				<fieldset id=\"inputs\" style=\"border:1px solid #128F9A\">
+					<legend>Offerta</legend><br />
 					<label>Offerte Privato</label>
 					<select name=\"OffertaId\">
 					<option>...</option>
@@ -62,12 +64,12 @@ if(isset($_POST['step'])){
 						echo "<option value=\"".$rsOfferte['OffertaId']."\">".$rsOfferte['OffertaNome']."</option>";
 						}
 				echo"</select>
-				<input id=\"IdCliente\" name=\"IdCliente\" type=\"hidden\" value=\"".$_POST['IdCliente']."\" >
-				<input id=\"ClienteTipologia\" name=\"ClienteTipologia\" type=\"hidden\" value=\"".$_POST['ClienteTipologia']."\" >
-				<input id=\"step\" name=\"step\" type=\"hidden\" value=\"3\" >
+					<input id=\"IdCliente\" name=\"IdCliente\" type=\"hidden\" value=\"".$_POST['IdCliente']."\" >
+					<input id=\"ClienteTipologia\" name=\"ClienteTipologia\" type=\"hidden\" value=\"".$_POST['ClienteTipologia']."\" >
+					<input id=\"step\" name=\"step\" type=\"hidden\" value=\"3\" >
 				</fieldset>
 				<fieldset id=\"actions\">
-				<input type=\"submit\" id=\"submit\" value=\"avanti\">
+					<input type=\"submit\" id=\"submit\" value=\"avanti\">
 				</fieldset>
 				</form>";
 				}
@@ -75,7 +77,8 @@ if(isset($_POST['step'])){
 				$sql = "SELECT * FROM Offerte WHERE TipologiaId = '".$_POST['TipologiaId']."' and OffertaDestinazione = 'Azienda'";
 				$res = mysql_query($sql);
 				echo"<form action=\"addcontratti.php\" method=\"post\">
-				<fieldset>
+				<fieldset id=\"inputs\" style=\"border:1px solid #128F9A\">
+					<legend>Offerta</legend><br />
 					<label>Offerte Azienda</label>
 					<select name=\"OffertaId\">
 					<option>...</option>
@@ -84,57 +87,99 @@ if(isset($_POST['step'])){
 						echo "<option value=\"".$rsOfferte['OffertaId']."\">".$rsOfferte['OffertaNome']."</option>";
 						}
 				echo"</select>
-				<input id=\"IdCliente\" name=\"IdCliente\" type=\"hidden\" value=\"".$_POST['IdCliente']."\" >
-				<input id=\"ClienteTipologia\" name=\"ClienteTipologia\" type=\"hidden\" value=\"".$_POST['ClienteTipologia']."\" >
-				<input id=\"step\" name=\"step\" type=\"hidden\" value=\"3\" >
+					<input id=\"IdCliente\" name=\"IdCliente\" type=\"hidden\" value=\"".$_POST['IdCliente']."\" >
+					<input id=\"ClienteTipologia\" name=\"ClienteTipologia\" type=\"hidden\" value=\"".$_POST['ClienteTipologia']."\" >
+					<input id=\"step\" name=\"step\" type=\"hidden\" value=\"3\" >
 				</fieldset>
 				<fieldset id=\"actions\">
-				<input type=\"submit\" id=\"submit\" value=\"avanti\">
+					<input type=\"submit\" id=\"submit\" value=\"avanti\">
 				</fieldset>
 				</form>";
 				}
 			break;
 		case 3:
-			echo "<h3>Completa Informazioni</h3>";
-			echo "<p>Completa il contratto in ogni sua parte;</p>";
+			echo "<h3>STEP 3 - Creazione Contratto</h3>";
 			$sql = "SELECT * FROM Offerte WHERE OffertaId = '".$_POST['OffertaId']."'";
 			$res = mysql_query($sql);
 			$rsOfferte = mysql_fetch_assoc($res);
-			echo "<table border=\"1\">
-					<tr>
-						<td>Nome</td>
-						<td>Canone</td>
-						<td>Fatturazione</td>
-					</tr>
-					<tr>
-						<td>".$rsOfferte['OffertaNome']."</td>
-						<td>".$rsOfferte['OffertaCanone']."</td>
-						<td>".$rsOfferte['OffertaPagamento']."</td>
-					</tr>
-					<tr>
-						<td colspan=\"3\">Descrizione</td>
-					</tr>
-					<tr>
-						<td colspan=\"3\">".$rsOfferte['OffertaDescrizione']."</td>
-					</tr>
-			</table>";
+			echo "
+			<fieldset id=\"inputs\" style=\"border:1px solid #128F9A\">
+					<legend>Dettaglio Offerta</legend><br />
+						<label>Nome</label>
+						<input type=\"text\" value=\"".$rsOfferte['OffertaNome']."\" readonly>
+						<label>Canone</label>
+						<input type=\"text\"value=\"".$rsOfferte['OffertaCanone']."\" readonly><br />
+						<label>Fatturazione</label>
+						<input type=\"text\" value=\"".$rsOfferte['OffertaPagamento']."\" readonly><br />
+						<label>Descrizione</label>
+						<textarea disabled rows=\"4\" cols=\"50\">".$rsOfferte['OffertaDescrizione']."</textarea>
+						<br />
+			</fieldset><br />";
 			$cliente = "SELECT * FROM Clienti where idCliente=".$_POST['IdCliente']."";
 			$resCliente = mysql_query($cliente);
 			$rsCliente = mysql_fetch_assoc($resCliente);
 			echo "
 				<form action=\"addcontratti.php\" method=\"post\">
-					<fieldset>
-						<h3>Dati Installazione</h3>
+					<fieldset id=\"inputs\" style=\"border:1px solid #128F9A\">
+					<legend>Dati Servizio</legend><br />
+					<div>
+						<a href=\"javascript:slideonlyone('boxattivazione');\" >Nuova Attivazione</a>
+					 </div>
+					 <div class=\"newboxes2\" id=\"boxattivazione\" style=\" display: none;\">
+						<label>Tipologia linea</label>
+						<select name=\"LineaDati\">
+							<option value=\"isdn\">ISDN</option>
+							<option value=\"pstn\">PSTN - POTS</option>
+							<option value=\"naked\">Solo Dati</option>
+							<option value=\"ibrida\">Dati & Fonia</option>
+						</select>
+						<br />
+						<label>Numero Pilota</label>
+						<input id=\"LineaPilota\" name=\"LineaPilota\" type=\"text\" placeholder=\"Numero Telefonico Adiacente\"  required>
+						<br />
+					</div>
+					<div>
+						<a href=\"javascript:slideonlyone('boxmigrazione');\" >Migrazione Linea</a>
+					 </div>
+					 <div class=\"newboxes2\" id=\"boxmigrazione\" style=\" display: none;\">
+						<label>Tipologia linea</label>
+						<select name=\"LineaDati\">
+							<option value=\"isdn\">ISDN</option>
+							<option value=\"pstn\">PSTN - POTS</option>
+							<option value=\"ibrida\">Dati & Fonia</option>
+						</select>
+						<br />
+						<label>Numero Linea</label>
+						<input id=\"LineaNumero\" name=\"LineaNumero\" type=\"text\" placeholder=\"Numero Telefonico da Migrare\"  required>
+						<br />
+					</div>
+					<br />
+					</fieldset>
+					<br />
+					<fieldset id=\"inputs\" style=\"border:1px solid #128F9A\">
+					<legend>Indirizzo Installazione</legend><br />
+						<label>Indirizzo</label>
 						<input id=\"ClienteIndirizzo1\" name=\"ClienteIndirizzo1\" type=\"text\" placeholder=\"Indirizzo\" value=\"".$rsCliente['ClienteIndirizzo']."\" required>
-						<input id=\"ClienteNumero1\" name=\"ClienteNumero1\" type=\"text\" placeholder=\"Numero Civico\" value=\"".$rsCliente['ClienteNumero']."\" required>
+						<label>Civico</label>
+						<input id=\"ClienteNumero1\" name=\"ClienteNumero1\" type=\"text\" placeholder=\"Numero Civico\" value=\"".$rsCliente['ClienteNumero']."\" required><br />
+						<label>C.A.P.</label>
 						<input id=\"ClienteCap1\" name=\"ClienteCap1\" type=\"text\" placeholder=\"C.A.P.\" value=\"".$rsCliente['ClienteCap']."\" required>
+						<label>Citt&agrave</label>
 						<input id=\"ClienteCitta1\" name=\"ClienteCitta1\" type=\"text\" placeholder=\"Citt&agrave\" value=\"".$rsCliente['ClienteCitta']."\" required>
-						<h3>Dati Fatturazione</h3>
-						<input id=\"ClienteIndirizzo2\" name=\"ClienteIndirizzo2\" type=\"text\" placeholder=\"Indirizzo\" value=\"".$rsCliente['ClienteIndirizzo']."\" required>
-						<input id=\"ClienteNumero2\" name=\"ClienteNumero2\" type=\"text\" placeholder=\"Numero Civico\" value=\"".$rsCliente['ClienteNumero']."\" required>
-						<input id=\"ClienteCap2\" name=\"ClienteCap2\" type=\"text\" placeholder=\"C.A.P.\" value=\"".$rsCliente['ClienteCap']."\" required>
-						<input id=\"ClienteCitta2\" name=\"ClienteCitta2\" type=\"text\" placeholder=\"Citt&agrave\" value=\"".$rsCliente['ClienteCitta']."\" required>
-						<h3>Dati Pagamento</h3>
+					</fieldset><br />
+					<fieldset id=\"inputs\" style=\"border:1px solid #128F9A\">
+					<legend>Indirizzo Fatturazione</legend><br />
+						<label>Indirizzo</label>
+						<input id=\"ClienteIndirizzo1\" name=\"ClienteIndirizzo1\" type=\"text\" placeholder=\"Indirizzo\" value=\"".$rsCliente['ClienteIndirizzo']."\" required>
+						<label>Civico</label>
+						<input id=\"ClienteNumero1\" name=\"ClienteNumero1\" type=\"text\" placeholder=\"Numero Civico\" value=\"".$rsCliente['ClienteNumero']."\" required><br />
+						<label>C.A.P.</label>
+						<input id=\"ClienteCap1\" name=\"ClienteCap1\" type=\"text\" placeholder=\"C.A.P.\" value=\"".$rsCliente['ClienteCap']."\" required>
+						<label>Citt&agrave</label>
+						<input id=\"ClienteCitta1\" name=\"ClienteCitta1\" type=\"text\" placeholder=\"Citt&agrave\" value=\"".$rsCliente['ClienteCitta']."\" required>
+					</fieldset><br />
+					<fieldset id=\"inputs\" style=\"border:1px solid #128F9A\">
+					<legend>Modalit&agrave Pagamento</legend>
 						<label>Pagamento Canone ".$rsOfferte['OffertaPagamento']."</label>
 						<select name=\"ContrattoPagamento\">
 							<option value=\"Bollettino\">Bollettino Postale</option>
@@ -152,8 +197,38 @@ if(isset($_POST['step'])){
 							<option value=\"Digitale\">Fattura su e-mail</option>
 							<option value=\"Cartaceo\">Fattura cartacea con addebito</option>
 						</select><br />
+					</fieldset>
+					<br />
+					<fieldset id=\"inputs\" style=\"border:1px solid #128F9A\">
+					<legend>Opzioni</legend><br />
+						<label>IP Statico</label>
+						<select name=\"OpzioneIp\">
+							<option value=\"1\">1 Ip Statico</option>
+							<option value=\"4\">4 Ip Statico</option>
+							<option value=\"8\">8 Ip Statico</option>
+							<option value=\"16\">16 Ip Statico</option>
+							<option value=\"32\">32 Ip Statico</option>
+						</select><br /><br />
+						<label style=\"float: left;\">Acquisto e installazione Access Point</label><input type=\"checkbox\" name=\"OpzioneAP\" value=\"1\" style=\"float: left;\">
+						<label style=\"float: rigth;\">Acquisto Switch 8 Porte</label><input type=\"checkbox\" name=\"OpzioneSwitch\" value=\"1\" style=\"float: rigth;\">
+						<br />
+						<br />
+						<br />
+						<label style=\"float: left;\">Pubblicazione Numero in Elenco Telefonico</label><input type=\"checkbox\" name=\"OpzioneElenco\" value=\"1\" style=\"float: left;\">
+						<label style=\"float: rigth;\">Identificativo Chiamante (Chi &egrave)</label><input type=\"checkbox\" name=\"OpzioneChie\" value=\"1\" style=\"float: rigth;\">
+						<br />
+						<br />
+						<br />
+						<label style=\"float: left;\">Blocco Identificativo Chiamante (CLI Nascosto)</label><input type=\"checkbox\" name=\"OpzioniClinascosto\" value=\"1\" style=\"float: left;\">
+						<label style=\"float: rigth;\">Trasferimento di Chiamata</label><input style=\"float: rigth;\" type=\"checkbox\" name=\"OpzioniTrasferimento\" value=\"1\">
+						<br />
+						<br />
+						<br />
+						<label style=\"float: left;\">Vuoi ricevere pubblicità?</label><input type=\"checkbox\" name=\"OpzioniPubblicita\" value=\"1\" style=\"float: left;\"><br />
+					</fieldset><br />
 						<input id=\"IdCliente\" name=\"IdCliente\" type=\"hidden\" value=\"".$_POST['IdCliente']."\" >
 						<input id=\"ClienteTipologia\" name=\"ClienteTipologia\" type=\"hidden\" value=\"".$_POST['ClienteTipologia']."\" >
+						<input id=\"OffertaId\" name=\"OffertaId\" type=\"hidden\" value=\"".$rsOfferte['OffertaId']."\" >
 						<input id=\"step\" name=\"step\" type=\"hidden\" value=\"4\" >
 					</fieldset>
 					<fieldset id=\"actions\">
@@ -164,6 +239,7 @@ if(isset($_POST['step'])){
 			print_r($_POST);
 			break;
 		case 4:
+			echo "<h3>STEP 4 - Creazione Contratto</h3>";
 			print_r($_POST);
 			break;
 	}
